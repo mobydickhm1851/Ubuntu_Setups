22c6d0ec1423f3960754cb97997acadaf96283c7
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-08-14 12:16:55 +0800

    Create list_for_useful_softwares.sh

---
+++ list_for_useful_softwares.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###--LaTeX equation on LibreOffice Impress--###
+###-----------------------------------------###
+#https://extensions.libreoffice.org/extensions/texmaths-1
+#Note:restart after adding the extenstion
+
+###--Visual Studio Code--###
+###----------------------###
+#https://code.visualstudio.com/download
+
+###--TexMaker--###
+###------------###
+sudo apt-get install texlive-full
+sudo apt-get install texmaker
+
+
