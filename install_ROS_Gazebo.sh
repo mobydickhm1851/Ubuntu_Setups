81d0e3329e3f033a6375edc8611ade73b4e84737
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-08-31 15:32:26 +0800

    Update install_ROS_Gazebo.sh

--- install_ROS_Gazebo.sh
+++ install_ROS_Gazebo.sh
@@ -10,8 +10,8 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 # first update
 sudo apt-get update
-# install full-desktop
-sudo apt-get install ros-kinetic-desktop-full
+# install full-desktop and rosbash(ros command)
+sudo apt-get install ros-kinetic-desktop-full ros-kinetic-rosbash
 # to find available packages
 # apt-cache search ros-kinetic
 
