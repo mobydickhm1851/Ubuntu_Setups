7983c3d32626da8f204fd0049183b527fecc8e97
mobydickhm1851 <37865338+mobydickhm1851@users.noreply.github.com>
2018-10-26 14:31:51 +0800

    Update solabot_persistent_usb.sh

--- solabot_persistent_usb.sh
+++ solabot_persistent_usb.sh
@@ -7,15 +7,14 @@
 # In the example "Bus 001 Device 005: ID 0403:6001" 0403 is the VendorID and 6001 the ProductID #
 
 # Create a file #
+# MODE:=\"0666\" for permission to bind the port
 sudo touch /etc/udev/rules.d/99-usb-serial.rules
 # Add arduino_nano (/dev/ttyUSB*) #
-sudo bash -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", SYMLINK+=\"solabot_nano\"' >> /etc/udev/rules.d/99-usb-serial.rules"
-# Add arduino_micro (/dev/ttyACM*) #
-sudo bash -c "echo 'KERNEL==\"ttyACM*\", ATTRS{idVendor}==\"2341\",SYMLINK+=\"solabot_micro\"' >> /etc/udev/rules.d/99-usb-serial.rules"
+sudo bash -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", MODE:=\"0666\", SYMLINK+=\"solabot_nano\"' >> /etc/udev/rules.d/99-usb-serial.rules"
 # Add rplidar (/dev/ttyUSB*) #
-sudo bash -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"10c4\", ATTRS{idProduct}==\"ea60\", SYMLINK+=\"rplidar\"' >> /etc/udev/rules.d/99-usb-serial.rules"
+sudo bash -c "echo 'SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"10c4\", ATTRS{idProduct}==\"ea60\", MODE:=\"0666\", SYMLINK+=\"rplidar\"' >> /etc/udev/rules.d/99-usb-serial.rules"
 # Add RAZOR_IMU_9DOF (/dev/ttyACM*) #
-sudo bash -c "echo 'KERNEL==\"ttyACM*\", ATTRS{idVendor}==\"1b4f\", ATTRS{idProduct}==\"9d0e\",SYMLINK+=\"solabot_razor\"' >> /etc/udev/rules.d/99-usb-serial.rules"
+sudo bash -c "echo 'KERNEL==\"ttyACM*\", ATTRS{idVendor}==\"1b4f\", ATTRS{idProduct}==\"9d0e\", MODE:=\"0666\",SYMLINK+=\"solabot_razor\"' >> /etc/udev/rules.d/99-usb-serial.rules"
 
 # load the new rule #
 sudo udevadm trigger
